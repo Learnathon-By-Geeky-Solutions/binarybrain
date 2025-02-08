5b39846df52555b6a3bd598336e5a55a9d0fb28f
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-02-08 07:43:31 +0600

    fix: SonarCloud security hotspot

--- online-classroom-management/src/test/java/com/onlineclassroom/management/service/UserServiceImplTest.java
+++ online-classroom-management/src/test/java/com/onlineclassroom/management/service/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -39,8 +38,6 @@ public class UserServiceImplTest {
 
     @BeforeEach
     void setUp(){
-        MockitoAnnotations.openMocks(this);
-
         userDto = new UserDto();
         userDto.setFirstName("Moinul");
         userDto.setLastName("Islam");
