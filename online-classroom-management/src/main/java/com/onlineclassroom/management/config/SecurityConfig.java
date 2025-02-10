137ebca007f991f67e3998c01f1c83e869a7b38f
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-02-10 15:18:52 +0600

    fix: enable csrf protection

--- online-classroom-management/src/main/java/com/onlineclassroom/management/config/SecurityConfig.java
+++ online-classroom-management/src/main/java/com/onlineclassroom/management/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.onlineclassroom.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -27,12 +26,9 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/user/register"))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/user/register").permitAll()
                         .anyRequest().authenticated()
                 );
 
