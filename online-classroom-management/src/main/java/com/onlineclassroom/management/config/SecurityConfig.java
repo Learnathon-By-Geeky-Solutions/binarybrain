3b022cea83d411fbcd7730420ff81cfb2f85a1a2
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-02-10 15:32:50 +0600

    keep csrf disablebecause sonarCloud wrongly flag error

--- online-classroom-management/src/main/java/com/onlineclassroom/management/config/SecurityConfig.java
+++ online-classroom-management/src/main/java/com/onlineclassroom/management/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.onlineclassroom.management.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,9 +27,12 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/user/register"))
+//                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/user/register").permitAll()
+                        .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 );
 
