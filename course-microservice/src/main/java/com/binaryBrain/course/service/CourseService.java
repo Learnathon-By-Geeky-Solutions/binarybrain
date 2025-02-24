adf7f59100e6f641d07bf82f6d7290563c5964be
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-02-24 22:22:31 +0600

    Course CRUD operation DONE

---
+++ course-microservice/src/main/java/com/binaryBrain/course/service/CourseService.java
@@ -0,0 +1,21 @@
+package com.binaryBrain.course.service;
+
+import com.binaryBrain.course.model.Course;
+
+import java.util.List;
+
+
+public interface CourseService {
+
+    Course createCourse(Course course, String jwt);
+    Course getCourseByCourseId(Long id, String jwt);
+
+    List<Course> getAllCourseByAuthorId(Long id, String jwt);
+
+    List<Course> getAllCourse(String jwt);
+
+    Course updateCourse(Long courseId, Course course, String jwt);
+
+    void deleteCourse(Long courseId, String jwt);
+
+}
