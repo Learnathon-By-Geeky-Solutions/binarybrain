2819fe8e112f213698dda19e2971952c5eebb9e3
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-02-25 09:50:26 +0600

    Modified Classroom code and added custom exception

---
+++ classroom-microservice/src/main/java/com/binaryBrain/classroom_microservice/service/CourseService.java
@@ -0,0 +1,15 @@
+package com.binaryBrain.classroom_microservice.service;
+
+import com.binaryBrain.classroom_microservice.dto.CourseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "Course-Microservice", url="http://localhost:5001/")
+public interface CourseService {
+    @GetMapping("/api/v1/private/course/{id}")
+    CourseDto getCourseById(@PathVariable Long id,
+                            @RequestHeader("Authorization") String jwt);
+
+}
