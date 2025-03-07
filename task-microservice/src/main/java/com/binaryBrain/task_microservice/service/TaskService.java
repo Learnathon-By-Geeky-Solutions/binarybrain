8b841c38f5ecc27391a15f5835303e11ae09e3d4
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-03-07 10:24:36 +0600

    Implement basic CRUD opreration for Task and modify Gateway

---
+++ task-microservice/src/main/java/com/binaryBrain/task_microservice/service/TaskService.java
@@ -0,0 +1,15 @@
+package com.binaryBrain.task_microservice.service;
+
+import com.binaryBrain.task_microservice.dto.TaskDto;
+
+import java.util.List;
+
+public interface TaskService {
+    TaskDto createTask(TaskDto taskDto, String username);
+    TaskDto getTaskById(Long id, String username);
+    List<TaskDto> getAllTaskByTeacherId(Long id, String username);
+    List<TaskDto> getTasksbyIds(List<Long> taskIds, String username);
+    TaskDto closeTask(Long taskId, String username);
+    TaskDto updateTask(Long taskId, TaskDto updatedTaskDto, String username);
+    void deleteTaskById(Long id, String username);
+}
