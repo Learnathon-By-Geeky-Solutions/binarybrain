5bba0dd38fd4576a44ca577f5d6a8e177e9576e3
MD. Moinul Islam Sourav <moinulmd306@gmail.com>
2025-04-03 00:50:42 +0600

    Remove security dependency from common-exception-handler

--- common-exception-handler/src/main/java/com/binarybrain/exception/global/GlobalExceptionHandler.java
+++ common-exception-handler/src/main/java/com/binarybrain/exception/global/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.binarybrain.exception.global;
 import com.binarybrain.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
